f9c2e69c446b7b14ee56320b83ffdab14665ff06
stefanyAA <stefanyapoloalvarez6@gmail.com>
2022-06-04 14:42:22 +0200

    working with fragments

--- app/src/main/java/com/example/myapplication/AnimalesPerdidos.java
+++ app/src/main/java/com/example/myapplication/AnimalesPerdidos.java
@@ -15,6 +15,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class AnimalesPerdidos extends AppCompatActivity {
 
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
 
 
 
@@ -22,8 +25,30 @@ public class AnimalesPerdidos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        tabLayout=findViewById(R.id.tabLayout);
+        viewPager=findViewById(R.id.viewPager);
+        viewPagerAdapter= new ViewPagerAdapter(this);
+        viewPager.setAdapter(viewPagerAdapter);
 
 
+
+
+    tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        @Override
+        public void onTabSelected(TabLayout.Tab tab) {
+            viewPager.setCurrentItem(tab.getPosition());
+        }
+
+        @Override
+        public void onTabUnselected(TabLayout.Tab tab) {
+
+        }
+
+        @Override
+        public void onTabReselected(TabLayout.Tab tab) {
+
+        }
+    });
 
     }
 }
