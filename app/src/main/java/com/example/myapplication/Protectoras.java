70062b36ee88ba045aebae6ae6be8e559cb541bc
MiriamVazDec <miriamdeclara@gmail.com>
2022-06-04 21:30:35 +0200

    lista protectoras

--- app/src/main/java/com/example/myapplication/Protectoras.java
+++ app/src/main/java/com/example/myapplication/Protectoras.java
@@ -36,40 +36,40 @@ public class Protectoras extends AppCompatActivity implements View.OnClickListen
         btnFloat.setOnClickListener((View.OnClickListener)this);
 
         listaProtectora= new ArrayList<>();
-        listaProtectora.add(new Protectora(getResources().getDrawable(R.drawable.zarpas_y_colmillos)," Zarpas y colmillos",
-                " 987987987",
+        listaProtectora.add(new Protectora(getResources().getDrawable(R.drawable.zarpas_y_colmillos),"Zarpas y colmillos",
+                "987987987",
                 "calle ni idea",
-                "64789",
+                "CP: 64789",
                 "https://www.zarpasycolmillos.es/"));
         listaProtectora.add(new Protectora(getResources().getDrawable(R.drawable.cipar),"CIPAR",
-                " 987987987",
+                "987987987",
                 "calle ni idea",
-                "64789",
+                "CP: 64789",
                 "https://www.facebook.com/centroCIPAR/"));
         listaProtectora.add(new Protectora(getResources().getDrawable(R.drawable.ascan),"ASCAN",
-                " 987987987",
+                "987987987",
                 "calle ni idea",
-                "64789",
+                "CP: 64789",
                 "https://ascan.com.es/"));
         listaProtectora.add(new Protectora(getResources().getDrawable(R.drawable.fapam),"F.A.P.A.M.",
-                " 987987987",
+                "987987987",
                 "calle ni idea",
-                "64789",
+                "CP: 64789",
                 "https://fapam.org/"));
         listaProtectora.add(new Protectora(getResources().getDrawable(R.drawable.apac),"APAC",
-                " 987987987",
+                "987987987",
                 "calle ni idea",
-                "64789",
+                "CP: 64789",
                 "https://www.facebook.com/apac.ciempozuelos.9"));
         listaProtectora.add(new Protectora(getResources().getDrawable(R.drawable.palevlas),"PALEVLAS",
-                " 987987987",
+                "987987987",
                 "calle ni idea",
-                "64789",
+                "CP: 64789",
                 "https://palevlasprotectora.es/"));
         listaProtectora.add(new Protectora(getResources().getDrawable(R.drawable.spap),"S.P.A.P",
                 "913119133",
                 "calle ni idea",
-                "64789",
+                "CP: 64789",
                 "http://www.spap.net/"));
 
             adapter = new Adapter(listaProtectora);
